676df6d49933e2568fefe7286ef1d6ec3cfd2e78
Michael Treser <armikael2@gmail.com>
2017-06-20 23:46:38 +0300

    Added courses model.

---
+++ src/app/courses/courses.model.ts
@@ -0,0 +1,15 @@
+export class Course {
+  constructor(public name: string, public author: string){}
+}
+
+// Alternative syntax
+
+// export class Courses {
+//   public name: string;
+//   public author: string;
+//
+//   constructor(name: string, author: string) {
+//     this.name = name;
+//     this.author = author;
+//   }
+// }
